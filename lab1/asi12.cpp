2f00abfaf280f35998f13a0d111414f70c33597b
Lalitiiti <156576234+Lalitiiti@users.noreply.github.com>
2024-01-14 15:30:31 +0530

    Add files via upload

---
+++ lab1/asi12.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int W;
+cin>>W;
+int w[n],v[n];
+for(int i=0;i<n;i++)cin>>w[i];
+for(int i=0;i<n;i++)cin>>v[i];
+int dp[W+1][n+1];
+for(int i=0;i<=W;i++)dp[i][0]=0;
+for(int i=0;i<=n;i++)dp[0][i]=0;
+for(int i=1;i<=W;i++){
+for(int j=1;j<=n;j++){
+   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+   if(w[j-1]<=i)dp[i][j]=max(dp[i][j],dp[i-w[j-1]][j-1]+v[j-1]);
+}
+}
+cout<<dp[W][n]<<endl;
+}
