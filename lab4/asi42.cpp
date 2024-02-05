2f4baaf1187328af5e690a85c3ae748f3100da80
Lalitiiti <156576234+Lalitiiti@users.noreply.github.com>
2024-02-05 15:21:56 +0530

    Update asi42.cpp

--- lab4/asi42.cpp
+++ lab4/asi42.cpp
@@ -1,79 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-double Me(vector<int>& array1, vector<int>& array2)
+double Me(vector<int>& nums1, vector<int>& nums2)
     {
-        int size=array1.size()+array2.size();
-        int n=array1.size();
-        int m=array2.size();
-        
-        int ans=0,prev=0;
-        int arr1_i=0,arr2_j=0;
-        int z=size/2;
-        int count=-1;
-        if(size%2!=0){
-            while(count!=z && arr1_i<n && arr2_j < m){
-            if(array1[arr1_i]<=array2[arr2_j]){
-                ans=array1[arr1_i];
-                arr1_i++;
-                count++;
-            }
-            else{
-                ans=array2[arr2_j];
-                arr2_j++;
-                count++;
-            }
-          }
-          if(arr1_i==n){
-              while(count!=z){
-                prev=ans;
-                ans=array2[arr2_j];
-                arr2_j++;
-                count++;
-              }
-          }
-          else if(arr2_j==m){
-              while(count!=z){
-                prev=ans;
-                ans=array1[arr1_i];
-                arr1_i++;
-                count++;
-              }
-          }
-          return ans;
+    int n1 = nums1.size(), n2 = nums2.size();
+
+    if (n1 > n2)
+        return Me(nums2, nums1);
+
+    int n = n1 + n2;
+    int left = (n1 + n2 + 1) / 2;
+    int low = 0, high = n1;
+
+    while (low <= high)
+    {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+
+        int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+
+        if (mid1 < n1)
+            r1 = nums1[mid1];
+        if (mid2 < n2)
+            r2 = nums2[mid2];
+        if (mid1 - 1 >= 0)
+            l1 = nums1[mid1 - 1];
+        if (mid2 - 1 >= 0)
+            l2 = nums2[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1)
+        {
+            if (n % 2 == 1)
+                return max(l1, l2);
+            else
+                return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
         }
-        else{
-            while(count!=z && arr1_i<n && arr2_j < m){
-            if(array1[arr1_i]<=array2[arr2_j]){
-                prev=ans;
-                ans=array1[arr1_i];
-                arr1_i++;
-                count++;
-            }
-            else{
-                prev=ans;
-                ans=array2[arr2_j];
-                arr2_j++;
-                count++;
-            }
-          }
-          if(arr1_i==n){
-              while(count!=z){
-                prev=ans;
-                ans=array2[arr2_j];
-                arr2_j++;
-                count++;
-              }
-          }
-          else if(arr2_j==m){
-              while(count!=z){
-                prev=ans;
-                ans=array1[arr1_i];
-                arr1_i++;
-                count++;
-              }
-          }
-          return (double)(ans+prev)/2;
+        else if (l1 > r2)
+        {
+            high = mid1 - 1;
         }
+        else
+        {
+            low = mid1 + 1;
+        }
+    }
+    return 0;
     }
 int main() {
     freopen("input42.txt","r",stdin);
@@ -85,4 +55,5 @@ int main() {
     for(int i=0;i<m;i++)cin>>b[i];
     cout<<Me(a,b)<<endl;
     return 0;
+}
 }
